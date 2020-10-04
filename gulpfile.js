35890aecadea9aa34b8519f9de4e169d3dee0ea8
mhe <markus.herhoffer@exentra.de>
2020-10-04 13:08:50 +0200

    linting for gulpfile

--- gulpfile.js
+++ gulpfile.js
@@ -1,27 +1,26 @@
-var gulp        = require('gulp');
+var gulp = require('gulp');
 var browserSync = require('browser-sync').create();
-var ftp         = require('vinyl-ftp');
-var logger      = require('gulplog');
+var ftp = require('vinyl-ftp');
+var logger = require('gulplog');
 
-// Static server
-gulp.task('dev', function() {
-    browserSync.init({
-        server: {
-            baseDir: "./"
-        }
-    });
-    gulp.watch("index.html").on('change', browserSync.reload);
+gulp.task('dev', function () {
+	browserSync.init({
+		server: {
+			baseDir: "./"
+		}
+	});
+	gulp.watch("index.html").on('change', browserSync.reload);
 });
 
-gulp.task('deploy', function() {
+gulp.task('deploy', function () {
 
-	var conn = ftp.create( {
-		host:       'vrsnsmv.com',
-		user:       '398303-ftp',
-		password:   'QFjqZ4DmaFyWQbjynJc',
-        parallel:   10,
-        log:        logger.info
-	} );
+	var conn = ftp.create({
+		host: 'vrsnsmv.com',
+		user: '398303-ftp',
+		password: 'QFjqZ4DmaFyWQbjynJc',
+		parallel: 10,
+		log: logger.info
+	});
 
 	var globs = [
 		'css/**',
@@ -31,7 +30,7 @@ gulp.task('deploy', function() {
 		'index.html'
 	];
 
-	return gulp.src( globs, { base: '.', buffer: false } )
-        .pipe( conn.newer( '/' ) )
-        .pipe( conn.dest( '/vrsnsmv' ) );
-} );
+	return gulp.src(globs, { base: '.', buffer: false })
+		.pipe(conn.newer('/'))
+		.pipe(conn.dest('/vrsnsmv'));
+});
